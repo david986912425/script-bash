d6a4611272ac26f7d8bb3f04ade07e9dec3e9b01
david968912425 <dm7659746@gmail.com>
2024-06-09 12:49:50 -0500

    download repository

--- download_repository.sh
+++ download_repository.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+cd ~/
+mkdir -p idbi
+cd idbi
+
 git clone git@github.com:idbi/id-pos.git
 
 git clone git@github.com:idbi/id-backend.git
@@ -13,5 +17,14 @@ git clone git@github.com:idbi/id-laradock.git
 cd id-laradock 
 git pull
 git checkout lite
-cd ..
+cd ~/imagesDocker
 
+docker load -i idbi-phpfpm.tar
+docker load -i idbi-mariadb.tar
+docker load -i idbi-nginx.tar
+docker load -i idbi-redis.tar
+docker load -i idbi-workspace.tar
+
+cd ~/idbi/id-laradock 
+
+docker compose up
