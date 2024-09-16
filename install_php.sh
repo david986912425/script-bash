80b64b5f8a1561e40723aaad97db9454a2879cb5
david986912425 <dm7659746@gmail.com>
2024-09-16 10:05:20 -0500

    add install php 8.1

--- install_php.sh
+++ install_php.sh
@@ -18,5 +18,5 @@ else
     sudo apt install php8.1 -y
     sudo apt install php8.1-cli php8.1-fpm php8.1-mysql php8.1-xml php8.1-mbstring php8.1-curl php8.1-zip php8.1-gd -y
 
-    echo "PHP 8.1 y las extensiones comunes se han instalado correctamente."
+    echo "PHP 8.1 se instalo correctamente."
 fi
